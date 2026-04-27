3efd611a4ab39bcace2803f9e739030b297d0f71
user <korundrom@gmail.com>
2026-04-27 20:36:17 +0400

    Изменён файл 6

--- task06.py
+++ task06.py
@@ -1,7 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 27 08:43:15 2026
+#Корунов, варианты 6, 20
+n = int(input("n = "))
+m = int(input("m = "))
 
-@author: linux
-"""
+A = []
+for i in range(n):
+    row = list(map(int, input(f"Введите значения строки {i + 1}, через пробел: ").split()))
+    A.append(row)
 
+b = 0
+
+for i in range(n):
+    maximum = max(A[i])
+    b += maximum
+
+print("Сумма:", b)
