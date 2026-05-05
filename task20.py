aa026ade9b9d20d545e98649d74facbdd6ba32e1
wdoin <korundrom@gmail.com>
2026-05-05 20:25:46 +0400

    20

--- task20.py
+++ task20.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 27 08:43:15 2026
+n = int(input("Введите размер квадртной матрицы: "))
 
-@author: linux
-"""
+A = []
+print("Матрица A:")
+for i in range(n):
+    row = list(map(float, input(f"Строка {i+1} матрицы A: ").split()))
+    A.append(row)
 
+B = []
+print("Матрица B:")
+for i in range(n):
+    row = list(map(float, input(f"Строка {i+1} матрицы B: ").split()))
+    B.append(row)
+
+proizv = []
+for i in range(n):
+    p = 1.0
+    for j in range(n):
+        p *= B[i][j]
+    proizv.append(p)
+
+C = []
+for i in range(n):
+    new_row = []
+    for j in range(n):
+        new_row.append(A[i][j] + proizv[i])
+    C.append(new_row)
+
+print("Новая матрица:")
+for i in C:
+    print(i)
+    
+    #Даны две действительные квадратные матрицы размером n*n. Получить новую
+#матрицу прибавлением к элементам каждого столбца первой матрицы
+#произведения элементов соответствующих строк второй матрицы
