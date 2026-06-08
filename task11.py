599c82b245b2bbc18aaf166b2297a4706a329ac9
Kamiromet <belyayeva_2004@inbox.ru>
2026-06-08 19:55:21 +0500

    Update task11.py
    Signed-off-by: Kamiromet <belyayeva_2004@inbox.ru>

--- task11.py
+++ task11.py
@@ -1,7 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 27 08:43:15 2026
+import random
 
-@author: linux
-"""
+str = int(input("Введите количество строк: "))
+stb = int(input("Введите количество столбцов: "))
 
+matrix = []
+for i in range(str):
+    row = []
+    for j in range(stb):
+        row.append(random.randint(-10, 10))
+    matrix.append(row)
+
+print("\nСгенерированная матрица:")
+for row in matrix:
+    print(*row)
+
+sums = [sum(row) for row in matrix]
+
+min_row = sums.index(min(sums))
+max_row = sums.index(max(sums))
+
+print("\nСтрока с минимальной суммой:")
+print(*matrix[min_row])
+print("Сумма =", sums[min_row])
+
+print("\nСтрока с максимальной суммой:")
+print(*matrix[max_row])
+print("Сумма =", sums[max_row])
