aaf2e5ddea0e471f8c8e8014e2402384bc6ed00b
Kamiromet <belyayeva_2004@inbox.ru>
2026-06-08 20:03:46 +0500

    Update task25.py
    Signed-off-by: Kamiromet <belyayeva_2004@inbox.ru>

--- task25.py
+++ task25.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 27 08:43:15 2026
+n = int(input("Введите размер матрицы: "))
 
-@author: linux
-"""
+matrix = []
+print("Введите элементы матрицы:")
+for i in range(n):
+    row = list(map(int, input().split()))
+    matrix.append(row)
 
+min_elem = matrix[0][0]
+col = 0
+
+for i in range(1, n):
+    if matrix[i][i] < min_elem:
+        min_elem = matrix[i][i]
+        col = i
+
+print("\nМатрица после удаления столбца:")
+for row in matrix:
+    for j in range(n):
+        if j != col:
+            print(row[j], end=" ")
+    print()
