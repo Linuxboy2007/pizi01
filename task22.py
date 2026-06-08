7ee568a69654d9b53ea0aa423ffe7da14d8cbd52
Annshap62 <amshapor@gmail.com>
2026-06-08 16:47:52 +0400

    Update task22.py
    Signed-off-by: Annshap62 <amshapor@gmail.com>

--- task22.py
+++ task22.py
@@ -1,7 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 27 08:43:15 2026
+5
+Шапоренко А.М. ПИЗИ23о1
+Дана действительная матрица размером n*m. Все элементы с наибольшим
+значением заменить нулями (таких элементов может быть несколько)
 
-@author: linux
 """
+def replace_max_in_rows(matrix):
+    for row in matrix:
+        if not row:          
+            continue
+        max_val = max(row)   
+        for i in range(len(row)):
+            if row[i] == max_val:
+                row[i] = 0.0
+    return matrix
 
+if "name" == "__main__":
+    try:
+        n = int(input("Введите количество строк (n): "))
+        m = int(input("Введите количество столбцов (m): "))
+
+        matrix = []
+        print("Введите элементы матрицы построчно (через пробел):")
+        for i in range(n):
+            row = list(map(float, input(f"Строка {i+1}: ").split()))
+            if len(row) != m:
+                print(f"Ошибка. Попробуйте снова.")
+                exit(1)
+            matrix.append(row)
+
+        print("Исходная матрица:")
+        for row in matrix:
+            print(" ".join(f"{x:.2f}" for x in row))
+
+        replace_max_in_rows(matrix)
+
+        print("Матрица конечная:")
+        for row in matrix:
+            print(" ".join(f"{x:.2f}" for x in row))
+
+    except ValueError:
+        print("Ошибка ввода: необходимо вводить числа.")
