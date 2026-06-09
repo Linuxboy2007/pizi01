3f65513852d0abf66fd0c7b52a5cb3883a6accab
yc-2603-660 <la4na@yandex.ru>
2026-06-09 11:24:48 +0400

    Добавил решение в 11

--- task11.py
+++ task11.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr 27 08:43:15 2026
+import random
 
-@author: linux
+n = int(input("Введите n: "))
+m = int(input("Введите m: "))
+matrix = [[random.randint(1, 100) for _ in range(m)] for _ in range(n)]
+
+maxx = -99999
+minn = 1000000
+for row in matrix:
+    row_sum = sum(row)
+    if row_sum > maxx:
+        maxx = row_sum
+    if row_sum < minn:
+        minn = row_sum
+
+print(matrix)
+print(maxx)
+print(minn)
+@author: Urusor
 """
 
