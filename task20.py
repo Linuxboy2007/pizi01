56048d65734b1edb9149040d3ba2182d44420ba7
Urus <la4na11@gmail.com>
2026-06-16 01:07:16 +0400

    Добавил решение в 20

--- task20.py
+++ task20.py
@@ -1,7 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 27 08:43:15 2026
+Семизов С.А. ПИЗИ23о1
+Даны две действительные квадратные матрицы размером n*n. Получить новую
+матрицу прибавлением к элементам каждого столбца первой матрицы
+произведения элементов соответствующих строк второй матрицы.
 
-@author: linux
 """
+import random
 
+n = int(input("Введите n: "))
+matrix1 = [[random.randint(1, 10) for _ in range(n)] for _ in range(n)]
+matrix2 = [[random.randint(1, 10) for _ in range(n)] for _ in range(n)]
+
+print("\nМатрица A:")
+for i in range(n):
+    for j in range(n):
+        print(f"{matrix1[i][j]:4d}", end="")
+    print()
+
+print("\nМатрица B:")
+for i in range(n):
+    for j in range(n):
+        print(f"{matrix2[i][j]:4d}", end="")
+    print()
+
+row_products = []
+for i in range(n):
+    product = 1
+    for j in range(n):
+        product *= matrix2[i][j]
+    row_products.append(product)
+
+
+result_matrix = []
+for i in range(n):
+    row = []
+    for j in range(n):
+        row.append(matrix1[i][j] + row_products[j])
+    result_matrix.append(row)
+
+print("\nНовая матрица C (A + произведение строк B):")
+for i in range(n):
+    for j in range(n):
+        print(f"{result_matrix[i][j]:6d}", end="")
+    print()
